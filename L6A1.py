67e175b2b0f3016ad326c359cb49c9566f5e9aef
Pristql <luigimansion111@gmail.com>
2026-05-04 20:28:10 +0530

    Add files via upload

---
+++ L6A1.py
@@ -0,0 +1,22 @@
+a=2
+b=7
+c=9
+
+if a and b and c:
+    print("All the numbers have boolean values as true")
+else:
+    print("All the numbers have boolean values as false")
+    
+d=3
+e=-4
+f=7
+
+if d>0 or e>0:
+    print("One number is greater than zero")
+else:
+    print("No number is greater than zero")
+    
+if e>0 or f>0:
+    print("One number is greater than zero")
+else:
+    print("No number is greater than zero")
